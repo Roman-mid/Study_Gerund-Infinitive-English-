e6c4222133762d4f5bbc6b9017508a8d1d3a394e
Roman-mid <roman.mid88@gmail.com>
2024-04-10 19:51:49 +0100

    firts commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,73 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { gerung, infinitive } from './words';
+
+
+enum Word {
+  GERUNG = 'Gerung',
+  INFINITIVE = 'Infinitive'
+}
 
 function App() {
+  
+  const [word, setWord] = React.useState<string>('Let\'s go start');
+  const [correct, setCorrect] = React.useState<string>('');
+  const [score, setScore] = React.useState<number>(0);
+
+  const getNumber = (num: number) => Math.floor(Math.random() * num);
+
+  React.useEffect(() => {
+    getWord()
+  }, [])
+
+  const getWord = () => {
+    const list = getNumber(2);
+    if(list === 0) {
+      const wordInd:number = getNumber(infinitive.length);
+      setWord(infinitive[wordInd])
+    } else {
+      const wordInd = getNumber(gerung.length);
+      setWord(gerung[wordInd])
+    }
+  };
+
+  const getResult = (categofy: string) => {
+    if(infinitive.includes(word) && categofy === Word.INFINITIVE) {
+      setCorrect('Correct');
+      setScore((prev) => prev + 1);
+    } else if (gerung.includes(word) && categofy === Word.GERUNG) {
+      setCorrect('Correct');
+      setScore((prev) => prev + 1);
+    } else {
+      setCorrect('False');
+      setScore(0);
+    };
+    setTimeout(() => {
+      getWord();
+      setCorrect('');
+
+    },1000 )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={correct === "Correct" ? "App correct" : correct === "False" ? "App noCorrect"  : 'App'}>
+      <div className="container">
+        <h1 className="title">What do you need use with word?</h1>
+        <h1 className="word">{word}</h1>
+        <div className="btnsWrap">
+          <button 
+            className="btn" 
+            onClick={() => getResult(Word.INFINITIVE)}>
+              {Word.INFINITIVE}
+            </button>
+          <button 
+            className="btn"
+            onClick={() => getResult(Word.GERUNG)}>
+              {Word.GERUNG}
+            </button>
+        </div>
+        <p className="score">{score}</p>
+      </div>
     </div>
   );
 }
